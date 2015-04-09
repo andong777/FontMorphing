c76876dd56878137a428452928ed8cb669a811ad
andong777 <andong777@163.com>
2015-04-09 17:34:31 +0800

    进行了较大的重构，主要是代码上的，后面会对数据结构进行重构。提取出了如下方法：getCorners、findStartPoint、getPolygon、plotPoints。

--- FontMorphingTool/FontMorphingTool.cpp
+++ FontMorphingTool/FontMorphingTool.cpp
@@ -23,11 +23,14 @@ static const int deltaAngle[8][2] = {{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1
 
 Point SGCPDPointToCVPoint(CPointDouble& cpdp);
 CPointDouble CVPointToSGCPDPoint(Point& cvp);
-
 Point Fade2DPointToCVPoint(Point2& f2dp);
 Point2 CVPointToFade2DPoint(Point& cvp);
 
 Mat fillHoles(Mat imInput);
+vector< vector<bool> > getCorners(const Mat& mat);
+Point findStartPoint(const Mat& mat);
+vector< vector<int> > getPolygon(Mat mat, Point start, vector<Point>& polygon);
+void plotPoints(vector<Point> points, Mat canvas, string title = "points", bool pause = true, Scalar color = Scalar(0, 0, 0));
 
 typedef struct Triangle {
 public:
@@ -207,120 +210,19 @@ void registerSourceChar(){
 		Mat edge;	// 边缘检测得到的矩阵，边缘为白色。
 		Canny(filled, edge, 40, 120);	// Canny方法输出灰度矩阵
 
-		// find corner points on the contour
-		bool **cornerFlag = new bool*[edge.size().height];
-		for (int i = 0; i<edge.size().height; i++){
-			cornerFlag[i] = new bool[edge.size().width];
-		}
-		for (int i = 0; i<edge.size().height; i++){
-			for (int j = 0; j<edge.size().width; j++){
-				cornerFlag[i][j] = false;
-			}
-		}
-		vector<Point> corners;
-		goodFeaturesToTrack(edge, corners, 20, .05, 20, Mat(), 5, true, .1);
-		cout << corners.size() << " corner points." << endl;
-		for (int i = 0; i<corners.size(); i++){
-			cornerFlag[corners[i].y][corners[i].x] = true;
-			corners[i].x += offsetX;
-			corners[i].y += offsetY;
-			circle(rgb, corners[i], 5, Scalar(255, 0, 0));
-		}
-		//imshow("source", rgb); waitKey();
+		auto cornerFlag = getCorners(edge);
+		cout << cornerFlag.size() << "!" << endl;
 
 		// contour line connection method
 		vector<Point> polygon;	//记录多边形上的点
-		int **orderAtThisPoint = new int*[edge.size().height];	// 记录点在多边形上的顺序，从1开始
-		for(int i=0;i<edge.size().height;i++){
-			orderAtThisPoint[i] = new int[edge.size().width];
-		}
-		for(int i=0;i<strokeImg.size().height;i++){
-			for(int j=0;j<strokeImg.size().width;j++){
-				orderAtThisPoint[i][j] = 0;
-			}
-		}
-
-		int currentX = -1, currentY = -1;
-		bool found = false;
-		for (int i = 0; i < edge.cols; i++){
-			for (int j = 0; j < edge.rows; j++){
-				if (edge.at<uchar>(j, i) > 128){
-					int firstStepCnt = 0, maxSecondStepCnt = 0;
-					for (int i = 0; i < 8; i++){
-						int tempX = i + deltaAngle[i][0];
-						int tempY = j + deltaAngle[i][1];
-						if (edge.at<uchar>(tempY, tempX) > 128){
-							firstStepCnt++;
-							int secondStepCnt = 0;
-							for (int j = 0; j<8; j++){
-								int tempXX = tempX + deltaAngle[j][0];
-								int tempYY = tempY + deltaAngle[j][1];
-								if (edge.at<uchar>(tempYY, tempXX) > 128){
-									secondStepCnt++;
-								}
-							}
-							if (secondStepCnt > maxSecondStepCnt)	maxSecondStepCnt = secondStepCnt;
-						}
-					}
-					if (firstStepCnt >= 1 && maxSecondStepCnt >= 1){
-						currentX = i; currentY = j;
-						found = true;
-						break;
-					}
-				}
-			}
-			if (found)	break;
-		}
+		// 寻找多边形的起始点
+		Point startPoint = findStartPoint(edge);
+		int currentX = startPoint.x, currentY = startPoint.y;
+		cout << "?(" << currentX << ", " << currentY << ")" << endl;
 		/*circle(rgb, Point(currentX+offsetX, currentY+offsetY), 2, Scalar(0, 0, 255), CV_FILLED);
 		cout << "current: (" << currentX+offsetX << ", " << currentY+offsetY << ")" << endl;
 		imshow("", rgb); waitKey();*/
-
-		while(edge.at<uchar>(currentY, currentX) > 128){
-			polygon.push_back(Point(currentX, currentY));
-			orderAtThisPoint[currentY][currentX] = polygon.size();
-			edge.at<uchar>(currentY, currentX) = 0;	// 标记此点已经用过，避免找回到此点
-			int firstStepCnt = 0;
-			int secondStepCnt[8] = {0};	// 标记选择一个点后，第二次能找到几个点
-			int firstStepDirection[8] = {0};
-			for(int i=0;i<8;i++){
-				int tempX = currentX + deltaAngle[i][0];
-				int tempY = currentY + deltaAngle[i][1];
-				if(edge.at<uchar>(tempY, tempX) > 128){
-					firstStepCnt++;
-					firstStepDirection[firstStepCnt-1] = i;
-					for(int j=0;j<8;j++){
-						int tempXX = tempX + deltaAngle[j][0];
-						int tempYY = tempY + deltaAngle[j][1];
-						if(edge.at<uchar>(tempYY, tempXX) > 128){
-							secondStepCnt[firstStepCnt-1]++;
-						}
-					}
-				}
-			}
-			if(firstStepCnt == 1){	// 只找到了一个后续点
-				currentX += deltaAngle[firstStepDirection[0]][0];
-				currentY += deltaAngle[firstStepDirection[0]][1];
-			}else if(firstStepCnt > 1){	// 多个可选的后续点
-				int minCnt = infinity, direct = -1;
-				for(int i=0;i<firstStepCnt;i++){
-					if(secondStepCnt[i] >= 1 && secondStepCnt[i] < minCnt){	// 找后续点最少的后续点？
-						minCnt = secondStepCnt[i];
-						direct = firstStepDirection[i];
-					}
-				}
-				currentX += deltaAngle[direct][0];
-				currentY += deltaAngle[direct][1];
-			}else{	// 没找到后续点，多边形的构建结束
-				cout << "find no next points" << endl;
-			}
-		}
-		/*for (int i = 0; i<polygon.size(); i++){
-			Point *p = &polygon[i];
-			p->x += offsetX;
-			p->y += offsetY;
-			circle(rgb, *p, 1, Scalar(0, 0, 0), CV_FILLED);
-		}
-		imshow("", rgb); waitKey();*/
+		getPolygon(edge, startPoint, polygon);
 
 		double sampleInterval = 100.;	// 每两个关键点间的大致距离
 		int numKeyPoints = floor(polygon.size() / sampleInterval);	// 根据上面距离，求出关键点个数，即采样段的个数
@@ -348,13 +250,12 @@ void registerSourceChar(){
 			}
 		}
 		// Plot the polygon
-		cout << samplePoints.size() << " sample points." << endl;
-		for(int i=0;i<samplePoints.size();i++){
+		for (int i = 0; i < samplePoints.size(); i++){	// large stroke image -> character image
 			Point *p = &samplePoints[i];
 			p->x += offsetX;
 			p->y += offsetY;
-			circle(rgb, *p, 2, Scalar(0, 0, 0));
 		}
+		plotPoints(samplePoints, rgb, "source", false);
 		for (int i = 0; i < samplePoints.size();i+=numSample){
 			circle(rgb, samplePoints[i], 5, Scalar(0, 0, 255));
 		}
@@ -402,13 +303,6 @@ void registerSourceChar(){
 		}
 		sourceCharVert.insert(sourceCharVert.end(), samplePoints.begin(), samplePoints.end());
 		imshow("source", rgb); waitKey();
-
-		for (int i = 0; i<edge.size().height; i++){
-			delete[] cornerFlag[i];
-			delete[] orderAtThisPoint[i];
-		}
-		delete[] cornerFlag;
-		delete[] orderAtThisPoint;
  	} // end of for stroke
 	for (vector<Triangle>::iterator it = connectTri.begin(); it != connectTri.end(); it++){
 		int a = (*it).va;	// because vertex starts from 1.
@@ -541,84 +435,16 @@ void registerTargetChar(){
 		cout << endl;
 
 		// find corner points on the contour
-		bool **cornerFlag = new bool*[edge.size().height];
-		for (int i = 0; i<edge.size().height; i++){
-			cornerFlag[i] = new bool[edge.size().width];
-		}
-		for (int i = 0; i<edge.size().height; i++){
-			for (int j = 0; j<edge.size().width; j++){
-				cornerFlag[i][j] = false;
-			}
-		}
-		vector<Point> corners;
-		goodFeaturesToTrack(edge, corners, 20, .05, 20, Mat(), 5, true, .1);
-		cout << corners.size() << " corner points." << endl;
-		for (int i = 0; i<corners.size(); i++){
-			cornerFlag[corners[i].y][corners[i].x] = true;
-			corners[i].x += offsetX;
-			corners[i].y += offsetY;
-			circle(rgb, corners[i], 5, Scalar(255, 0, 0));
-		}
-		imshow("target", rgb); waitKey();
+		auto cornerFlag = getCorners(edge);
 
 		// contour line connection method
 		vector<Point> polygon;	//记录多边形上的点
-		int **orderAtThisPoint = new int*[edge.size().height];	// 记录点在多边形上的顺序
-		for (int i = 0; i<edge.size().height; i++){
-			orderAtThisPoint[i] = new int[edge.size().width];
-		}
-		for (int i = 0; i<strokeImg.size().height; i++){
-			for (int j = 0; j<strokeImg.size().width; j++){
-				orderAtThisPoint[i][j] = 0;
-			}
-		}
-		for (int i = 0; i<keyPoints.size(); i++){	// char image -> large stroke image
+		for (int i = 0; i<keyPoints.size(); i++){	// character image -> large stroke image
 			keyPoints[i].x -= offsetX;
 			keyPoints[i].y -= offsetY;
 		}
-		int currentX = keyPoints[0].x;
-		int currentY = keyPoints[0].y;
-		while (edge.at<uchar>(currentY, currentX) > 128){
-			polygon.push_back(Point(currentX, currentY));
-			orderAtThisPoint[currentY][currentX] = polygon.size();
-			edge.at<uchar>(currentY, currentX) = 0;	// 标记此点已经用过，避免找回到此点
-			int firstStepCnt = 0;
-			int secondStepCnt[8] = { 0 };	// 标记选择一个点后，第二次能找到几个点
-			int firstStepDirection[8] = { 0 };
-			for (int i = 0; i<8; i++){
-				int tempX = currentX + deltaAngle[i][0];
-				int tempY = currentY + deltaAngle[i][1];
-				if (edge.at<uchar>(tempY, tempX) > 128){
-					firstStepCnt++;
-					firstStepDirection[firstStepCnt - 1] = i;
-					for (int j = 0; j<8; j++){
-						int tempXX = tempX + deltaAngle[j][0];
-						int tempYY = tempY + deltaAngle[j][1];
-						if (edge.at<uchar>(tempYY, tempXX) > 128){
-							secondStepCnt[firstStepCnt - 1]++;
-						}
-					}
-				}
-			}
-			if (firstStepCnt == 1){	// 只找到了一个后续点
-				currentX += deltaAngle[firstStepDirection[0]][0];
-				currentY += deltaAngle[firstStepDirection[0]][1];
-			}
-			else if (firstStepCnt > 1){	// 多个可选的后续点
-				int minCnt = infinity, direct = -1;
-				for (int i = 0; i<firstStepCnt; i++){
-					if (secondStepCnt[i] >= 1 && secondStepCnt[i] < minCnt){	// 找后续点最少的后续点？
-						minCnt = secondStepCnt[i];
-						direct = firstStepDirection[i];
-					}
-				}
-				currentX += deltaAngle[direct][0];
-				currentY += deltaAngle[direct][1];
-			}
-			else{
-				cout << "find no next points" << endl;
-			}
-		}
+		auto orderAtThisPoint = getPolygon(edge, keyPoints[0], polygon);	// 记录点在多边形上的顺序
+
 		vector<Point> samplePoints;	// 采样后得到的点
 		for (int i = 0; i<keyPoints.size(); i++){
 			int i2 = (i + 1) % keyPoints.size();	// 下一个关键点
@@ -682,23 +508,15 @@ void registerTargetChar(){
 		}
 
 		// Plot the polygon
-		cout << samplePoints.size() << " sample points." << endl;
-		for (int i = 0; i<samplePoints.size(); i++){
+		for (int i = 0; i < samplePoints.size(); i++){	// large stroke image -> character image
 			Point *p = &samplePoints[i];
 			p->x += offsetX;
 			p->y += offsetY;
-			circle(rgb, *p, 2, Scalar(0, 0, 0), CV_FILLED);
 		}
+		plotPoints(samplePoints, rgb, "target", true);
 		targetCharVert.insert(targetCharVert.end(), samplePoints.begin(), samplePoints.end());
-		imshow("target", rgb); waitKey();
 
 		delete[] vertexBelongToTemplate;
-		for (int i = 0; i<edge.size().height; i++){
-			delete[] cornerFlag[i];
-			delete[] orderAtThisPoint[i];
-		}
-		delete[] cornerFlag;
-		delete[] orderAtThisPoint;
 	}
 }
 
@@ -867,23 +685,19 @@ int main( int, char** argv )
 }
 
 Point SGCPDPointToCVPoint(CPointDouble& cpdp){
-	Point cvp(cpdp.x, cpdp.y);
-	return cvp;
+	return Point(cpdp.x, cpdp.y);
 }
 
 CPointDouble CVPointToSGCPDPoint(Point& cvp){
-	CPointDouble cpdp(cvp.x, cvp.y);
-	return cpdp;
+	return CPointDouble(cvp.x, cvp.y);
 }
 
 Point Fade2DPointToCVPoint(Point2& f2dp){
-	Point cvp(f2dp.x(), f2dp.y());
-	return cvp;
+	return Point(f2dp.x(), f2dp.y());
 }
 
 Point2 CVPointToFade2DPoint(Point& cvp){
-	Point2 f2dp(cvp.x, cvp.y);
-	return f2dp;
+	return Point2(cvp.x, cvp.y);
 }
 
 Mat fillHoles(Mat imInput){
@@ -899,4 +713,121 @@ Mat fillHoles(Mat imInput){
     imFilledHoles = imFilledHoles > 0;
     imShow.release();
     return imFilledHoles;
+}
+
+vector< vector<bool> > getCorners(const Mat& mat){
+	vector< vector<bool> > cornerFlag(mat.size().height);
+	for (int i = 0; i < cornerFlag.size(); i++){
+		cornerFlag[i] = vector<bool>(mat.size().width);
+	}
+	for (int i = 0; i<mat.size().height; i++){
+		for (int j = 0; j<mat.size().width; j++){
+			cornerFlag[i][j] = false;
+		}
+	}
+	vector<Point> corners;
+	goodFeaturesToTrack(mat, corners, 20, .05, 20, Mat(), 5, true, .1);
+	cout << corners.size() << " corner points." << endl;
+	for (int i = 0; i<corners.size(); i++){
+		cornerFlag[corners[i].y][corners[i].x] = true;
+		//circle(mat, corners[i], 5, Scalar(255, 0, 0));
+	}
+	//imshow("corners", mat); waitKey();
+	return cornerFlag;
+}
+
+Point findStartPoint(const Mat& mat){
+	for (int i = 0; i < mat.cols; i++){
+		for (int j = 0; j < mat.rows; j++){
+			if (mat.at<uchar>(j, i) > 128){
+				int firstStepCnt = 0, maxSecondStepCnt = 0;
+				for (int d = 0; d < 8; d++){
+					int tempX = i + deltaAngle[d][0];
+					int tempY = j + deltaAngle[d][1];
+					if (mat.at<uchar>(tempY, tempX) > 128){
+						firstStepCnt++;
+						int secondStepCnt = 0;
+						for (int dd = 0; dd<8; dd++){
+							int tempXX = tempX + deltaAngle[dd][0];
+							int tempYY = tempY + deltaAngle[dd][1];
+							if (mat.at<uchar>(tempYY, tempXX) > 128){
+								secondStepCnt++;
+							}
+						}
+						if (secondStepCnt > maxSecondStepCnt)	maxSecondStepCnt = secondStepCnt;
+					}
+				}
+				if (firstStepCnt >= 1 && maxSecondStepCnt >= 1){
+					return Point(i, j);
+				}
+			}
+		}
+	}
+	return Point(-1, -1);
+}
+
+vector< vector<int> > getPolygon(Mat mat, Point start, vector<Point>& polygon){
+	// todo: 这样找到的多边形有些角点并不在上面！
+	vector< vector<int> > orderAtThisPoint(mat.size().height);
+	for (int i = 0; i < orderAtThisPoint.size(); i++){
+		orderAtThisPoint[i] = vector<int>(mat.size().width);
+	}
+	for (int i = 0; i < mat.size().height; i++){
+		for (int j = 0; j < mat.size().width; j++){
+			orderAtThisPoint[i][j] = 0;
+		}
+	}
+	int currentX = start.x;
+	int currentY = start.y;
+	while (mat.at<uchar>(currentY, currentX) > 128){
+		polygon.push_back(Point(currentX, currentY));
+		orderAtThisPoint[currentY][currentX] = polygon.size();
+		mat.at<uchar>(currentY, currentX) = 0;	// 标记此点已经用过，避免找回到此点
+		int firstStepCnt = 0;
+		int secondStepCnt[8] = { 0 };	// 标记选择一个点后，第二次能找到几个点
+		int firstStepDirection[8] = { 0 };
+		for (int i = 0; i<8; i++){
+			int tempX = currentX + deltaAngle[i][0];
+			int tempY = currentY + deltaAngle[i][1];
+			if (mat.at<uchar>(tempY, tempX) > 128){
+				firstStepCnt++;
+				firstStepDirection[firstStepCnt - 1] = i;
+				for (int j = 0; j<8; j++){
+					int tempXX = tempX + deltaAngle[j][0];
+					int tempYY = tempY + deltaAngle[j][1];
+					if (mat.at<uchar>(tempYY, tempXX) > 128){
+						secondStepCnt[firstStepCnt - 1]++;
+					}
+				}
+			}
+		}
+		if (firstStepCnt == 1){	// 只找到了一个后续点
+			currentX += deltaAngle[firstStepDirection[0]][0];
+			currentY += deltaAngle[firstStepDirection[0]][1];
+		}
+		else if (firstStepCnt > 1){	// 多个可选的后续点
+			int minCnt = infinity, direct = -1;
+			for (int i = 0; i<firstStepCnt; i++){
+				if (secondStepCnt[i] >= 1 && secondStepCnt[i] < minCnt){	// 找后续点最少的后续点？
+					minCnt = secondStepCnt[i];
+					direct = firstStepDirection[i];
+				}
+			}
+			currentX += deltaAngle[direct][0];
+			currentY += deltaAngle[direct][1];
+		}
+		else{	// 没找到后续点，多边形的构建结束
+			cout << "find no next points" << endl;
+		}
+	}
+	return orderAtThisPoint;
+}
+
+void plotPoints(vector<Point> points, Mat canvas, string title, bool pause, Scalar color){
+	cout << points.size() << " points in " << title << "." << endl;
+	for (int i = 0; i < points.size(); i++){
+		circle(canvas, points[i], 2, Scalar(0, 0, 0));
+	}
+	imshow(title, canvas); 
+	if(pause)	waitKey();
 }
